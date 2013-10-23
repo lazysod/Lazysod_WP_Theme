2844ea9344759fda6a4c7bda93ae5b2dad8cb1a2
Barry Smith <divinorum2001@gmail.com>
2013-10-23 01:23:21 +0100

    first upload
    Upload of development files

---
+++ footer.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains footer content and the closing of the
+ * #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<div class="site-info">
+			
+			<p align="center">Copyright <?php echo date('Y'); ?> <a href="http://www.barrysmith.org" title="My home page">Barry Smith</a></div>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+
+
+<?php wp_footer(); ?>
+</body>
+</html>
